ed4c76bd3dd361210742b8ebab33ce3cbaf1808c
formateur-glodie-tshimini <contact@tshimini.fr>
2026-03-16 15:14:51 +0100

    feat: demo web server

---
+++ 0-demos/7-web-server/index.mjs
@@ -0,0 +1,28 @@
+import { createServer } from 'node:http'
+
+/**
+ * Depuis le lancement du script dans les arguments
+ * s'il y a le port en argument au rand 2 on prend sa valeur
+ * sinon on prend par defaut 7000
+ */
+let port = process.argv[2]
+if(!/\d{4}/.test(port)) {
+  port = 7000
+}
+createServer((req, res) => {
+  console.log('req', req.url, req.method)
+  if(req.url === '/' && req.method.toUpperCase() === 'GET') {
+    // Ecrire dans les headers
+    res.writeHead(200)
+    // Ecrire dans le body
+    res.write('<h1>Bienvenue</h1>')
+    res.write('<p>A toi jeune Dev Node</p>')
+    // Fermer le flux d'écriture et retourner une réponse
+    res.end()
+    // Alternative des lignes 18, 19 et 21 res.end('Bienvenue a toi jeune Dev Node')
+  } else {
+    res.end('404 not found')
+  }
+}).listen(port, () => {
+  console.info(`Running at http://localhost:${port}`)
+})
