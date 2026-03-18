c60c3ecb92d4bb654e424ffb623b703b2563df04
formateur-glodie-tshimini <contact@tshimini.fr>
2026-03-18 18:52:34 +0100

    fix: missing tests/

--- 1-ateliers/corrections/8.1/maths.mjs
+++ 1-ateliers/corrections/8.1/maths.mjs
@@ -3,3 +3,8 @@ export const sum = (...args) => {
   args.forEach(nb => sum += nb)
   return sum
 }
+
+export const divide = (nb1, nb2) => {
+  if(nb2 == '0') throw new Error('Divide by 0 impossible')
+  return nb1/nb2
+}
